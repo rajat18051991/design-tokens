0bb312a9150278b5c8fdcabb11f26bc91e995f91
rajat18051991 <rajat.kshyp@yahoo.com>
2025-04-18 12:51:50 +0000

    Initial setup with Style Dictionary

---
+++ build/ios/StyleDictionaryColor.h
@@ -0,0 +1,37 @@
+
+//
+// StyleDictionaryColor.h
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, ) {
+ColorAsphaltPrimaryBackgroundLow,
+ColorAsphaltPrimaryContentLow,
+ColorAsphaltPrimaryContentMedium,
+ColorAsphaltPrimaryBackgroundHigh,
+ColorAsphaltPrimaryContentHigh,
+ColorCrimsonNegativeBackgroundLow,
+ColorCrimsonNegativeBackgroundHigh,
+ColorCrimsonNegativeContent,
+FontH1FontSize,
+FontH1FontWeight,
+FontH1LineHeight,
+FontH1FontFamily,
+FontH2FontSize,
+FontH2FontWeight,
+FontH2LineHeight,
+FontH2FontFamily,
+FontBodyFontSize,
+FontBodyFontWeight,
+FontBodyLineHeight,
+FontBodyFontFamily
+};
+
+@interface : NSObject
++ (NSArray *)values;
++ (UIColor *)color:()color;
+@end
